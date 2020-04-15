dc6f2bca0f3d57ea41e62cd31af8d1a9778e27e8
thunderBiscuit <thunderB@protonmail.com>
2020-04-15 13:07:48 -0400

    docs: add config file + website MVP

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Tiny Node Docs",
+  description: "Documentation website for tiny node",
+  base: "/tiny-node-docs/",
+  themeConfig: {
+    navbar: true,
+    nav: [
+      { text: "GitHub", link: "https://github.com/thunderBiscuit/tiny-node-docs"},
+      { text: "Website", link: "https://www.thunderbiscuit.com/"}
+    ],
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# Tiny Node Documentation
